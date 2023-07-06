f437dcb045cba389d0edfd374e195bb39bf7aabe
cbbalboni <camila.balboni@gmail.com>
2023-07-06 12:45:42 -0300

    2 files modified, 19 added

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,12 @@
-import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-@Component({
-  selector: 'my-app',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <body>
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-      <section id=tweets><div class=content><p>tweets</p></div></section>
-      <section id=favorites><div class=content><p>favorites</p></div></section>
-      <section id=conversations><div class=content><p>conversations</p></div></section>
-      <section id=retweets><div class=content><p>retweets</p></div></section>
-
-    </body>
-  `,
-})
-export class App {
-  name = 'Angular';
+if (environment.production) {
+    enableProdMode();
 }
 
-bootstrapApplication(App);
+platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
